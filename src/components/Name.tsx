91628c6094fcbda7cd591a992d20db69647f7a05
Andrew Hyndman <ajhyndman@hotmail.com>
2020-05-23 19:05:11 -0700

    Make useFragment a little more robust

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,5 +1,6 @@
 import {DocumentNode, gql} from 'apollo-boost';
 import React from 'react';
+import {invariant} from 'ts-invariant';
 
 import {useApolloClient} from '@apollo/react-hooks';
 
@@ -15,15 +16,40 @@ export const VIEWER_FRAGMENT = gql`
   }
 `;
 
-const useFragment = (id: string, fragment: DocumentNode) => {
+const useFragment = ({
+  fragment,
+  id,
+  fragmentName,
+}: {
+  fragment: DocumentNode;
+  id: string;
+  fragmentName: string;
+}) => {
   const client = useApolloClient();
-  const typeName = fragment.definitions[0]?.typeCondition?.name?.value;
-  return client.readFragment({id: `${typeName}:${id}`, fragment});
+  let definition;
+  if (fragmentName != null) {
+    definition = fragment.definitions.find(
+      // @ts-ignore: Third party types for Document definitions are imprecise
+      (definition) => definition?.name?.value === fragmentName
+    );
+    invariant(
+      definition?.kind === 'FragmentDefinition',
+      `Could not find a fragment matching name: ${fragmentName}`
+    );
+  } else {
+    definition = fragment.definitions[0];
+    invariant(
+      definition?.kind === 'FragmentDefinition',
+      `No fragmentName was supplied, so we looked at the first definition, but it was a ${definition?.kind}, not a FragmentDefinition`
+    );
+  }
+  // @ts-ignore: Third party types for Document definitions are imprecise
+  const typeName = definition?.typeCondition?.name?.value;
+  return client.readFragment({id: `${typeName}:${id}`, fragment, fragmentName});
 };
 
 const Name = ({id}: Props) => {
-  const viewer = useFragment(id, VIEWER_FRAGMENT);
-  console.log({viewer});
+  const viewer = useFragment({id, fragment: VIEWER_FRAGMENT, fragmentName: 'ViewerFragment'});
 
   return <p>You are authenticated as: {viewer?.name}</p>;
 };
