7d44d1e21646ec74eaa22198746b5578e0e5ed31
Andrew Hyndman <ajhyndman@hotmail.com>
2020-05-24 13:28:38 -0700

    Lift useFragment into it's own file

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,8 +1,7 @@
-import {DocumentNode, gql} from 'apollo-boost';
+import {gql} from 'apollo-boost';
 import React from 'react';
-import {invariant} from 'ts-invariant';
 
-import {useApolloClient} from '@apollo/react-hooks';
+import {useFragment} from '../util/useFragment';
 
 type Props = {
   id: string;
@@ -10,43 +9,9 @@ type Props = {
 
 export const VIEWER_FRAGMENT = gql`
   fragment ViewerFragment on User {
-    id
-    login
     name
   }
 `;
-
-const useFragment = ({
-  fragment,
-  id,
-  fragmentName,
-}: {
-  fragment: DocumentNode;
-  id: string;
-  fragmentName: string;
-}) => {
-  const client = useApolloClient();
-  let definition;
-  if (fragmentName != null) {
-    definition = fragment.definitions.find(
-      // @ts-ignore: Third party types for Document definitions are imprecise
-      (definition) => definition?.name?.value === fragmentName
-    );
-    invariant(
-      definition?.kind === 'FragmentDefinition',
-      `Could not find a fragment matching name: ${fragmentName}`
-    );
-  } else {
-    definition = fragment.definitions[0];
-    invariant(
-      definition?.kind === 'FragmentDefinition',
-      `No fragmentName was supplied, so we looked at the first definition, but it was a ${definition?.kind}, not a FragmentDefinition`
-    );
-  }
-  // @ts-ignore: Third party types for Document definitions are imprecise
-  const typeName = definition?.typeCondition?.name?.value;
-  return client.readFragment({id: `${typeName}:${id}`, fragment, fragmentName});
-};
 
 const Name = ({id}: Props) => {
   const viewer = useFragment({id, fragment: VIEWER_FRAGMENT, fragmentName: 'ViewerFragment'});
---
+++ src/util/useFragment.ts
@@ -0,0 +1,36 @@
+import {DocumentNode} from 'apollo-boost';
+import invariant from 'ts-invariant';
+
+import {useApolloClient} from '@apollo/react-hooks';
+
+export const useFragment = ({
+  fragment,
+  id,
+  fragmentName,
+}: {
+  fragment: DocumentNode;
+  id: string;
+  fragmentName: string;
+}) => {
+  const client = useApolloClient();
+  let definition;
+  if (fragmentName != null) {
+    definition = fragment.definitions.find(
+      // @ts-ignore: Third party types for Document definitions are imprecise
+      (definition) => definition?.name?.value === fragmentName
+    );
+    invariant(
+      definition?.kind === 'FragmentDefinition',
+      `Could not find a fragment matching name: ${fragmentName}`
+    );
+  } else {
+    definition = fragment.definitions[0];
+    invariant(
+      definition?.kind === 'FragmentDefinition',
+      `No fragmentName was supplied, so we looked at the first definition, but it was a ${definition?.kind}, not a FragmentDefinition`
+    );
+  }
+  // @ts-ignore: Third party types for Document definitions are imprecise
+  const typeName = definition?.typeCondition?.name?.value;
+  return client.readFragment({id: `${typeName}:${id}`, fragment, fragmentName});
+};
